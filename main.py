33274e0eb5f56a68cd02b3e475cb7618543966d3
tinka1110 <kotituok@arcada.fi>
2025-05-04 15:13:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ rooms = [
     {"room_number": 103, "room_type": "Suite", "price": 200},
 ]
 
-
-bookings = []
+# bookings
+bookings = [
+    {"id": 1, "room_number": 101, "booking_date": "2025-05-01", "guest_name": "John Doe", "stars": None},
+    {"id": 2, "room_number": 102, "booking_date": "2025-05-02", "guest_name": "Jane Smith", "stars": None},
+]
 
 # Skapar FastAPI
 app = FastAPI()
@@ -20,23 +23,23 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Tillåter alla domäner
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],  # Tillåter alla HTTP-metoder
-    allow_headers=["*"],  # Tillåter alla headers
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
 
-# 
+
 @app.get("/")
 def hello():
     return {"message": "Hello FastAPI"}
 
-# GET /rooms 
+# GET /rooms
 @app.get("/rooms")
 async def get_rooms():
     return rooms
 
-# GET /bookings 
+# GET /bookings
 @app.get("/bookings")
 async def get_bookings():
     return bookings
@@ -52,15 +55,31 @@ async def create_booking(request: Request):
 
     # Skapa bokningen
     new_booking = {
+        "id": len(bookings) + 1,  
         "room_number": data["room_number"],
         "booking_date": data["booking_date"],
-        "guest_name": data["guest_name"]
+        "guest_name": data["guest_name"],
+        "stars": None  # Startar utan stjärnor
     }
 
-    
     bookings.append(new_booking)
 
     return {"message": "Bokningen har sparats!"}
+
+# PUT /bookings/
+@app.put("/bookings/{id}")
+async def update_booking(id: int, stars: int):
+    # Stjärnorna är mellan 1 och 5
+    if stars < 1 or stars > 5:
+        return {"message": "Stjärnorna måste bli mellan 1-5."}
+
+    # Sök efter bokningen med det angivna ID:t
+    for booking in bookings:
+        if booking["id"] == id:
+            booking["stars"] = stars  
+            return {"message": "Recension uppdaterad"}
+    
+    return {"message": "Bokning inte hittad"}
 
 
 if __name__ == "__main__":
