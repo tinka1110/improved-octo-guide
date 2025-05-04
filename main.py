b4fbe1f8750db3a3c6ab2ad0fac9cb93015bf563
tinka1110 <kotituok@arcada.fi>
2025-05-04 14:27:45 +0300

    /rooms

--- main.py
+++ main.py
@@ -2,14 +2,28 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-PORT=5000
+PORT = 5000
 
+# Example room
+rooms = [
+    {"room_number": 101, "room_type": "Single", "price": 100},
+    {"room_number": 102, "room_type": "Double", "price": 150},
+    {"room_number": 103, "room_type": "Suite", "price": 200},
+]
+
+#api
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
+# api
 @app.get("/")
 def hello():
     return { "message": "Hello FastAPI" }
+
+# /rooms
+@app.get("/rooms")
+async def get_rooms():
+    return rooms
 
 if __name__ == "__main__":
     uvicorn.run(
